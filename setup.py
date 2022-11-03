7e1c2610d070674087c5a4266debf4a863517085
Jannik Schmied <jannik.schmied@pm.me>
2022-11-03 12:05:42 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="python-rts", 
+      version="0.1", 
+      description="A package for Real Time Systems calculations", 
+      url="https://github.com/j-schmied",
+      author="Jannik Schmied",
+      author_email="jannik.schmied@stud.htw-dresden.de",
+      license="MIT",
+      packages=["rts"],
+      zip_safe=False)
