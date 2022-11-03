101af76b0f099ffe613e5fa8cd88abd74d3705ac
Jannik Schmied <jannik.schmied@pm.me>
2022-11-03 21:09:31 +0100

    Merged setup.py and requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools import setup
+from distutils.core import setup
+from setuptools import find_packages
 
 setup(name="python-rts", 
-      version="0.1", 
+      version="0.1",
+      packages=find_packages(),
+      package_data={p: ['*'] for p in find_packages()},
       description="A package for Real Time Systems calculations", 
       url="https://github.com/j-schmied",
       author="Jannik Schmied",
       author_email="jannik.schmied@stud.htw-dresden.de",
       license="MIT",
+      install_requires=[
+            "numpy"
+      ],
       packages=["rts"],
-      zip_safe=False)
+      zip_safe=False
+)
